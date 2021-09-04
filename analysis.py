17060bf2056772ba1050099959ccb7c2757c9017
Pavel Strakhov <p.strakhov4work@gmail.com>
2021-09-04 18:18:33 +0300

    Delete all empty rows. Change gender to f and m. Fill gender for prenatal hospitals with 'f' value, fill other NaN columns with 0 value

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 pd.set_option('display.max_columns', 8)
 general = pd.read_csv('test/general.csv')
 prenatal = pd.read_csv('test/prenatal.csv')
@@ -9,4 +10,13 @@ prenatal.columns = general.columns
 sports.columns = general.columns
 result_df = pd.concat([general, prenatal, sports], ignore_index=True)
 result_df.drop(columns=['Unnamed: 0'], inplace=True)
+
+result_df.dropna(how='all', inplace=True)
+result_df['gender'] = result_df['gender'].replace(['man', 'male', 'woman', 'female'], ['m', 'm', 'f', 'f'])
+
+result_df[result_df.hospital == 'prenatal'] = result_df[result_df.hospital == 'prenatal'].fillna({'gender': 'f'})
+result_df = result_df.fillna({'bmi': 0, 'diagnosis': 0, 'blood_test': 0, 'ecg': 0, 'ultrasound': 0, 'mri': 0, 'xray': 0,
+                              'children': 0, 'months': 0})
+print(f'Data shape: {result_df.shape}')
 print(result_df.sample(n=20, random_state=30))
+
