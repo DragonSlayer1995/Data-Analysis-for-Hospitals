0b11f0aaacc6bbc2519ef4c4a1ebd956a6bf69bc
Pavel Strakhov <p.strakhov4work@gmail.com>
2021-09-04 16:42:43 +0300

    Change column names and merge three data sets

--- analysis.py
+++ analysis.py
@@ -1,11 +1,12 @@
 import pandas as pd
-
 pd.set_option('display.max_columns', 8)
-
 general = pd.read_csv('test/general.csv')
 prenatal = pd.read_csv('test/prenatal.csv')
 sports = pd.read_csv('test/sports.csv')
-
-print(general.head(20))
-print(prenatal.head(20))
-print(sports.head(20))
+# prenatal.rename(columns={'HOSPITAL': 'hospital', 'Sex': 'gender'}, inplace=True)
+# sports.rename(columns={'Hospital': 'hospital', 'Male/female': 'gender'}, inplace=True)
+prenatal.columns = general.columns
+sports.columns = general.columns
+result_df = pd.concat([general, prenatal, sports], ignore_index=True)
+result_df.drop(columns=['Unnamed: 0'], inplace=True)
+print(result_df.sample(n=20, random_state=30))
