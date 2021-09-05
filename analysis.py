80acb4d42b766b6473ad9de1d12bfd9a0eba7d81
Pavel Strakhov <p.strakhov4work@gmail.com>
2021-09-05 21:49:41 +0300

    Answers to questions

--- analysis.py
+++ analysis.py
@@ -17,6 +17,27 @@ result_df['gender'] = result_df['gender'].replace(['man', 'male', 'woman', 'fema
 result_df[result_df.hospital == 'prenatal'] = result_df[result_df.hospital == 'prenatal'].fillna({'gender': 'f'})
 result_df = result_df.fillna({'bmi': 0, 'diagnosis': 0, 'blood_test': 0, 'ecg': 0, 'ultrasound': 0, 'mri': 0, 'xray': 0,
                               'children': 0, 'months': 0})
-print(f'Data shape: {result_df.shape}')
-print(result_df.sample(n=20, random_state=30))
 
+general_hospital_patients_count = len(result_df[result_df.hospital == 'general'])
+general_hospital_stomach_patients_count = len(
+    result_df[(result_df.hospital == 'general') & (result_df.diagnosis == 'stomach')])
+
+sports_hospital_patients_count = len(result_df[result_df.hospital == 'sports'])
+sports_hospital_dislocation_patients_count = len(
+    result_df[(result_df.hospital == 'sports') & (result_df.diagnosis == 'dislocation')])
+
+general_patients_median_age = result_df.loc[result_df.hospital == 'general', 'age'].median()
+sports_patients_median_age = result_df.loc[result_df.hospital == 'sports', 'age'].median()
+
+blood_test_count_table = pd.pivot_table(result_df[result_df.blood_test == 't'], values='blood_test', index='hospital',
+                                        aggfunc='count')
+print(f"The answer to the 1st question is {result_df['hospital'].value_counts().idxmax()}")
+print(f"The answer to the 2nd question is "
+      f"{round((general_hospital_stomach_patients_count / general_hospital_patients_count), 3)}")
+print(f"The answer to the 3rd question is "
+      f"{round(sports_hospital_dislocation_patients_count / sports_hospital_patients_count, 3)}")
+
+print(f"The answer to the 4th question is {general_patients_median_age - sports_patients_median_age}")
+print(
+    f"The answer to the 5th question is {blood_test_count_table['blood_test'].idxmax()},"
+    f" {blood_test_count_table['blood_test'].max()} blood tests")
